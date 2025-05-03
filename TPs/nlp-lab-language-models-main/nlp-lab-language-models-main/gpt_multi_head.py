021727fc95753e937921bd5c84fa4a7816621797
nourhez09 <nourhezbri.prepa@gmail.com>
2025-05-04 01:40:49 +0200

    Add TPs to the repo

---
+++ TPs/nlp-lab-language-models-main/nlp-lab-language-models-main/gpt_multi_head.py
@@ -0,0 +1,229 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+# hyperparameters
+batch_size = 32
+block_size = 8
+max_iters = 5000
+eval_interval = 500
+learning_rate = 1e-3
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embd = 32
+n_heads = 4
+dropout = 0.2
+
+# ------------
+
+torch.manual_seed(2023)
+
+# Load the Victor Hugo dataset
+with open('data/hugo_contemplations.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+# here are all the unique characters that occur in this text
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+# Train and test splits
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
+
+# data loading
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+class Head(nn.Module):
+    """ one head of self-attention """
+
+    def __init__(self, head_size):
+        super().__init__()
+        # YOUR CODE
+        # add you key, query and value definitions
+        self.head_size = head_size        
+        # Définir les couches Key, Query, et Value comme des couches linéaires
+        self.key = nn.Linear(n_embd,head_size)
+        self.query = nn.Linear(n_embd,  head_size)
+        self.value = nn.Linear(n_embd,  head_size)
+        self.dropout= nn.Dropout(dropout)
+        ###
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+
+
+    def forward(self, x):
+        B,T,C = x.shape
+        ## YOUR CODE HERE
+        k = self.key(x)  
+        q = self.query(x) 
+        v = self.value(x)
+        weight = torch.matmul(q, k.transpose(1, 2))
+        weight = weight.masked_fill(self.tril == 0, float('-inf'))
+        weight = torch.nn.functional.softmax(weight, dim=-1)
+        weight= self.dropout(weight)
+        ###
+        out = weight @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
+        return out
+
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        ## YOUR CODE HERE
+        ## list of num_heads modules of type Head
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)]) 
+        self.dropout= nn.Dropout(dropout)
+        ###
+        
+    def forward(self, x):
+        ## YOUR CODE HERE
+        ## apply each head in self.heads to x and concat the results 
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out= self.dropout(out)
+        return out
+
+
+class FeedForward(nn.Module):
+    """ a simple MLP with RELU """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU(),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Block(nn.Module):
+    """ A single bloc of multi-head attention """
+
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.layer_norm1= nn.LayerNorm(n_embd)
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.layer_norm2= nn.LayerNorm(n_embd)
+        self.ffwd = FeedForward(n_embd)
+
+    def forward(self, x):
+        # x = self.sa(x)
+        # x = self.ffwd(x)
+        x= self.layer_norm1(x)
+        x = x + self.sa(x)
+        x=self.layer_norm2(x)
+        x = x + self.ffwd(x)
+        return x
+
+class BigramLanguageModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        # self.sa_head = MultiHeadAttention(n_heads, n_embd // n_heads)
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+        # self.ff = FeedForward(n_embd)
+        self.att=nn.Sequential(*[Block(n_embd, n_heads) for _ in range(3)])
+        self.layer_norm= nn.LayerNorm(n_embd)
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+
+        # idx and targets are both (B,T) tensor of integers
+        tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+        x = tok_emb + pos_emb # (B,T,C)
+        # x = self.sa_head(x) # (B,T,C)
+        # x = self.ff(x)
+        x=self.att(x)
+        x=self.layer_norm(x)
+        logits = self.lm_head(x) # (B,T,vocab_size)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
+
+model = BigramLanguageModel()
+m = model.to(device)
+# print the number of parameters in the model
+print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
+
+# create a PyTorch optimizer
+optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+
+    # every once in a while evaluate the loss on train and val sets
+    if iter % eval_interval == 0 or iter == max_iters - 1:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = m(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+
+# generate from the model
+prompt = torch.tensor(encode(['\n']), device=device)
+context = torch.ones((1,8), dtype=torch.long, device=device)*prompt
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
